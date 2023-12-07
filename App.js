12900ea8b2aeb825562a18ba05efbab123edc68a
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-12-07 22:34:39 +0530

    Day 2: L02 - Igniting our App
    Learnt:
    * About npm
    * About package.json/package-lock.json
    * About why apps are fast
    * About bundlers
    * About parcel and its Superpowers
    * About browserslist
    * About Behind-the-scenes of create-react-app

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
-/**
- * nested element
- * <div id="parent">
- *      <div id="child1">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h1 tag</h2>
- *      </div>
- *      <div id="child2">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement OR React.createElement => Object => render() => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h1", {}, "This is namaste react"),
     React.createElement("h2", {}, "I'm h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
@@ -24,12 +11,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I'm h2 tag"),
   ]),
 ]);
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React"
-// );
 
 console.log(parent);
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+## Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm => written using C++
+- Caching => Faster builds
+- Image Optimization
+- Minification of files while bundling
+- Compresses file
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling => support for older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking => Remove unused code for us
+- Different dev and prod bundles
