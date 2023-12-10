304192b653584b72c7daf45f6410323a08f55c28
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-12-10 21:14:58 +0530

    Learnt about JSX and Babel

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "This is namaste react"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-]);
+// React Element => Object => HTML Element(render)
 
-console.log(parent);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Namaste React by Aditya 🚀"
+// );
+
+// JSX is not HTML or XML in JS
+// JSX is HTML-like or XML-like syntax but not equal to HTML or XML
+
+// JSX Element => JSX Code is transpiled by PARCEL(with help of Babel) before it reaches the JS Engine
+// meaning it is converted into a code which is understood by the JS Engine
+
+// JSX => React.createElement => JS Object => HTML Element(render)
+const jsxHeading = <h1 id="heading">H1 tag using JSX!</h1>;
+
+// console.log(heading);
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxHeading);
