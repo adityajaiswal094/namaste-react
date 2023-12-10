58fdd9069c7938c4d9a560d85db06dd49b728417
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-12-10 22:59:53 +0530

    Day 3: L03 - Laying the foundation
    * Learnt the features and beauty of JSX

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element => Object => HTML Element(render)
+const Title = () => <h1 id="heading">H1 tag using JSX!</h1>;
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Namaste React by Aditya 🚀"
-// );
+const num = 10000;
 
-// JSX is not HTML or XML in JS
-// JSX is HTML-like or XML-like syntax but not equal to HTML or XML
+// JSX prevents cross-site scripting attacks - it sanitizes the data before running it
 
-// JSX Element => JSX Code is transpiled by PARCEL(with help of Babel) before it reaches the JS Engine
-// meaning it is converted into a code which is understood by the JS Engine
-
-// JSX => React.createElement => JS Object => HTML Element(render)
-const jsxHeading = <h1 id="heading">H1 tag using JSX!</h1>;
-
-// console.log(heading);
-console.log(jsxHeading);
+// React Functional Component
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h2>{num}</h2>
+    {100 + 200}
+    <h1 className="heading">Namaste React Functional Component 🚀</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
